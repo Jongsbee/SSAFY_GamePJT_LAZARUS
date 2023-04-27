cb6797a8ca058cee509d9fe703117358e2a0d17e
SadoRuin <dope2514@gmail.com>
2023-04-27 19:53:38 +0900

    :sparkles: [BE] LogServer 브랜치 분할하고 다시 푸시

---
+++ api-server/LogServer/src/test/java/com/exodia/logserver/LogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exodia.logserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

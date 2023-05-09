6da11934c03903e335c66c14215f072d934111f1
LeeSH <mylifeforzug123@gmail.com>
2023-05-09 17:42:06 +0900

    :bug: [BE] 커밋안된 파일 재커밋
    - 커밋안된거 같인 커뮛합니돠

---
+++ scheduler-server/SchedulerServer/src/main/java/com/exodia/schedulerserver/SchedulerServerApplication.java
@@ -0,0 +1,13 @@
+package com.exodia.schedulerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerServerApplication.class, args);
+	}
+
+}

eb9a6077757d8d7507ac1f14743890949afa04f8
박윤환 <dope2514@gmail.com>
2023-05-15 15:33:42 +0900

    :rocket: [FE] 빌드 오류 수정

--- front/admin-front/vue.config.js
+++ front/admin-front/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+    publicPath: './',
     transpileDependencies: true,
     devServer: {
         port: 3001, // 원하는 포트 번호로 변경

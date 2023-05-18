430dba47276bb1a01852e25b6b2b26aa44a36f2d
lsh <cxzlkjhgfdsa0821@gmail.com>
2023-05-19 08:56:50 +0900

    :recycle: [FE] history 모드 오류 수정

--- front/admin-front/vue.config.js
+++ front/admin-front/vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    publicPath: './',
+    publicPath: "./",
     transpileDependencies: true,
     devServer: {
         port: 3001, // 원하는 포트 번호로 변경
+        historyApiFallback: true,
     },
 });
